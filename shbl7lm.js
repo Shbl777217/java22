abb000da4caf8e27f161301ab2a3ceb0edfc7e6e
Shbl777217 <115428778+Shbl777217@users.noreply.github.com>
2022-11-15 20:45:11 +0300

    Update shbl7lm.js

--- shbl7lm.js
+++ shbl7lm.js
@@ -245,3 +245,47 @@ d2.hlm2.flex-grow-1.break.light{
         } 
     }, 10);
   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(`<style>.nosel.d-flex .room.bord {border-inline: 4px solid #00bbea !important;}
+
+.nosel.d-flex.room {display: inline-block;
+  background-image: url(https://up6.cc/2022/10/166724712939571.gif);
+
+background-size: 100%; 
+border-radius: 0px 0px 25px 25px;
+box-shadow: 0px 0px 4px 4px #00eaff38;
+}
+
+.nosel.d-flex.room .dots.mini.u-msg {text-align: center;background-size: 200% auto; -webkit-background-clip: text;-webkit-text-fill-color: #0000;background-image: url(https://j.top4top.io/p_2495q9u9q1.gif);}
+
+.nosel.d-flex.room .fitimg.u-pic.borderg{border-radius: 100px;border: 2px solid #00bbea;letter-spacing: 1px;background-size: 108% 108%;-webkit-text-fill-color: white;animation: grad 2s cubic-bezier(.445,.05,.55,.95) alternate infinite;}
+
+
+
+.nosel.d-flex.room .label-primary {background-color: transparent !important;etter-spacing: 1px;-webkit-text-fill-color: #000;padding: 0px !important;border-radius: 50px !important;}
+
+
+
+
+ 
+.nosel.d-flex.room .corner.fa.fa-user.label.uc.fa-microphone.label-danger {background-color: transparent;letter-spacing: 1px;-webkit-text-fill-color: #000;padding: 0px !important;border-radius: 50px !important;background-color: #transparent!important;}
+
+.nosel.d-flex.room .u-topic.dots {text-transform: uppercase;background-image: linear-gradient(-225deg, #000 0%, #00bbea 29%, #007aad 67%, #000 100%);background-size: 220% auto;-webkit-background-clip: text;-webkit-text-fill-color: transparent;animation: textclip 2s linear infinite;display: inline-block;}
+@keyframes textclip {to {background-position: 200% center;}}</style>`)
+
